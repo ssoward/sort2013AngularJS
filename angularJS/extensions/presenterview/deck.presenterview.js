8b695b5077a6381e6fd722ec8edfce4bc1ab8e4a
Scott Soward <amorvivir@yahoo.com>
2013-10-15 22:07:08 -0600

    fixed bug.

--- angularJS/extensions/presenterview/deck.presenterview.js
+++ angularJS/extensions/presenterview/deck.presenterview.js
@@ -11,7 +11,7 @@ if (window.$ !== undefined) {
 //	var presenter = window.open(presenterView.getLinkToPresenterView(),
 //                                    'deck.js - presenterView',
 //                                    'width=' + screen.width + ', height=' + screen.height);
-//    });
+    });
 
     /**
     * Note: This event is also triggered when the presentation is loaded!
